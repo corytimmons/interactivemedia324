c36f018f8d50bd05b24f92ba9c318ff4ecae3f5e
corytimmons <43824202+corytimmons@users.noreply.github.com>
2019-03-10 23:40:30 -0700

    Add files via upload

---
+++ portfolio/assets/js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+  $(".a").click(function(){
+    $(".imagea").show();
+    $(".b").show();
+  });
+  $(".b").click(function(){
+    $(".imageb").show();
+    $(".c").show();
+  });
+  $(".c").click(function(){
+    $(".imagec").show();
+    $(".d").show();
+  });
+  $(".d").click(function(){
+    $(".imaged").show();
+    $(".e").show();
+  });
+            $(".e").click(function(){
+  $(".imagee").show();
+    $(".f").show();
+  });
+  $(".f").click(function(){
+    $(".imagef").show();
+    $(".g").show();
+  });
+    $(".g").click(function(){
+    $(".imageg").show();
+    $(".h").show();
+  });
+    $(".h").click(function(){
+    $(".imageh").show();
+    $(".i").show();
+  });
+      $(".i").click(function(){
+    $(".imagei").show();
+    $(".j").show();
+  });
+        $(".j").click(function(){
+    $(".imagej").show();
+    $(".k").show();
+  });
+          $(".k").click(function(){
+    $(".imagek").show();
+    $(".l").show();
+  });
+            $(".l").click(function(){
+    $(".imagel").show();
+    $(".m").show();
+  });
+$(".m").click(function(){
+    $(".imagem").show();
+    $(".n").show();
+  });
+$(".n").click(function(){
+    $(".imageo").show();
+    $(".p").show();
+  });
+$(".p").click(function(){
+    $(".imagep").show();
+    $(".q").show();
+  });
+  $(".q").click(function(){
+    $(".imager").show();
+    $(".r").show();
+  });
+$("#featuredgallery").click(function(){
+    $("#featuredphotos").show();
+    $("#featureddetails").show();
+  });
+
+
+});
