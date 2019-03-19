6f5611d5221108c10db736b2289f13c76daa03fc
corytimmons <43824202+corytimmons@users.noreply.github.com>
2019-03-19 10:19:37 -0700

    final portfolio

---
+++ project3/assets/js/script_new.js
@@ -0,0 +1,136 @@
+$(document).ready(function(){
+    $(".reset").click(function(){
+    $(".item1").hide();
+    $(".item2").hide();
+    $(".item3").hide();
+    $(".item4").hide();
+    $(".item5").hide();
+    $(".item6").hide();
+    $(".text2").hide();
+    $(".item7").hide();
+    $(".item8").hide();
+    $(".item9").hide();
+    $(".item10").hide();
+    $(".item11").hide();
+    $(".item12").hide();
+  });
+
+ $(".expand").click(function(){
+    $(".item1").show();
+    $(".item2").show();
+    $(".item3").show();
+    $(".item4").show();
+    $(".item5").show();
+    $(".item6").show();
+    $(".text2").show();
+    $(".item7").show();
+    $(".item8").show();
+    $(".item9").show();
+    $(".item10").show();
+    $(".item11").show();
+    $(".item12").show();
+  });
+
+//  $("img[src='./images/expand.svg']").click(function () {
+//      $(this).attr("src","./images/expanded.svg");
+//      $("img[src='./images/reseted.svg").attr("src","./images/reset.svg");
+//  })
+// $("img[src='./images/expanded.svg']").click(function () {
+//      $(this).attr("src","./images/expand.svg");
+//      $("img[src='./images/reseted.svg").attr("src","./images/reset.svg");
+//  })
+// $("img[src='./images/reset.svg']").click(function () {
+//      $(this).attr("src","./images/reseted.svg");
+//      $("img[src='./images/expand.svg").attr("src","./images/expanded.svg");
+//  })
+// $("img[src='./images/reseted.svg']").click(function () {
+//      $(this).attr("src","./images/reset.svg");
+//      $("img[src='./images/expanded.svg").attr("src","./images/expand.svg")
+//  })
+
+ $(".artist").click(function(){
+    $(".item1").show();
+    $(".item2").show();
+    $(".item3").show();
+    $(".item7").show();
+    $(".item8").show();
+        $(".item9").show();
+  });
+
+$(".designer").click(function(){
+    $(".item4").show();
+    $(".item5").show();
+    $(".item6").show();
+    $(".item10").show();
+    $(".item11").show();
+    $(".item12").show();
+  });
+
+  $(".video").click(function(){
+    $(".item2").show();
+    $(".item3").show();
+  });
+
+  $(".writing").click(function(){
+    $(".item13").show();
+    $(".item14").show();
+    $(".item15").show();
+  });
+
+$(".netart").click(function(){
+    $(".item7").show();
+    $(".item8").show();
+    $(".item9").show();
+    $(".item10").show();
+    $(".item11").show();
+    $(".item12").show();
+  });
+
+$(".drag").click(function(){
+    $(".item7").show();
+  });
+
+// $(".collage").click(function(){
+//     $(".item4").show();
+//     $(".item5").show();
+//   });
+
+$(".trauma").click(function(){
+    $(".item1").show();
+    $(".item3").show();
+    $(".item12").show();
+  });
+
+$(".family").click(function(){
+    $(".item1").show();
+    $(".item3").show();
+  });
+
+$(".gender").click(function(){
+    $(".item2").show();
+    $(".item3").show();
+    $(".item7").show();
+  });
+
+$(".baroque").click(function(){
+    $(".baroquetext").toggle();
+  });
+
+
+$(".secondstory").click(function(){
+    $(".secondtext").toggle();
+  });
+
+$(".thirdstory").click(function(){
+    $(".thirdtext").toggle();
+  });
+  //  $(".text2").click(function(){
+  //   $(".item7").show();
+  //   $(".item8").show();
+  //   $(".item9").show();
+  //   $(".item10").show();
+  //   $(".item11").show();
+  //   $(".item12").show();
+  //   $(".text3").show();
+  //   $(".text4").show();
+  });
